3ab1d3cfff74a64c2dc397e5ad7f7b5e8b197afa
Nikolas Smith <smitniko@sheridancollege.ca>
2020-09-28 20:29:05 -0400

    Init commit.

---
+++ src/pickacard/NewClass.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pickacard;
+
+/**
+ *
+ * @author Nikol
+ */
+public class NewClass {
+    
+    System.out.println("New Class!");
+}
