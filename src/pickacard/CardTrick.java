98dc7b5591e348f30ea87643eb9785d120aceb35
selvakus <71720660+selvakus@users.noreply.github.com>
2020-10-04 23:16:27 -0400

    Update CardTrick.java

--- src/pickacard/CardTrick.java
+++ src/pickacard/CardTrick.java
@@ -55,7 +55,31 @@ public class CardTrick {
             System.out.println("User card found in magicHand");
         else
             System.out.println("User card not found in magicHand");
-    }
+    
+
+
+       boolean found = false;
+       // create a luckyCard
+       Card luckyCard = new Card();
+       luckyCard.setValue(9);
+       luckyCard.setSuit(Card.SUITS[2]);
+      
+       // loop to determine if luckyCard is present in magicHand
+       for(int i=0;i<magicHand.length;i++)
+       {
+           if(magicHand[i].getValue() == luckyCard.getValue() && magicHand[i].getSuit().equalsIgnoreCase(luckyCard.getSuit()))
+           {
+               found = true;
+               break;
+           }
+       }
+      
+       // display the result based on the search
+       if(found)
+           System.out.println("Lucky card found in magicHand");
+       else
+           System.out.println("Lucky card not found in magicHand");
+   }
 
 }
 
